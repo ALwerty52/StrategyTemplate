86e68a42a234c2b6226d85faf527ccfe5415f5c7
AL <alena7gur@gmail.com>
2025-07-26 10:08:29 +0300

    twoo

--- bridge/strategy/strategy.py
+++ bridge/strategy/strategy.py
@@ -18,6 +18,7 @@ class Strategy:
         self,
     ) -> None:
         self.we_active = False
+        self.i = 0
 
     def process(self, field: fld.Field) -> list[Optional[Action]]:
         """Game State Management"""
@@ -81,6 +82,48 @@ class Strategy:
         - actions[9] = Actions.BallGrab(0.0)
                 The robot number 9 grabs the ball at an angle of 0.0 (it looks to the right, along the OX axis)
         """
+        # c = (aux.dist(field.enemies[0].get_pos(), field.allies[0].get_pos()))/8
+        # d = aux.point_on_line(field.allies[0].get_pos(), field.enemies[0].get_pos(), c)
 
-        actions[5] = Actions.GoToPointIgnore(field.enemies[5].get_pos(), (field.enemies[5].get_pos() - field.allies[5].get_pos()).arg())
+        # u = aux.rotate(aux.Point(500, 0), ((3.14/4)*3) + 3.14 + (time())/3)
+        # actions[3] = Actions.GoToPointIgnore(field.ball.get_pos() + u, (field.ball.get_pos() - field.allies[3].get_pos()).arg())
+           
+        # if(self.i == 0):
+        #     actions[3] = Actions.GoToPointIgnore(field.enemy_goal.frw_up, 1)
+        #     if((field.allies[3].get_pos() - field.enemy_goal.frw_up).mag() < 250):
+        #         self.i = 1
+        # if(self.i == 1):
+        #     actions[3] = Actions.GoToPointIgnore(field.enemy_goal.frw_down, 1)
+        #     if((field.allies[3].get_pos() - field.enemy_goal.frw_down).mag() < 250):
+        #         self.i = 2  
+        # if(self.i == 2):
+        #     actions[3] = Actions.GoToPointIgnore(field.ally_goal.frw_up, 1)
+        #     if((field.allies[3].get_pos() - field.ally_goal.frw_up).mag() < 250):
+        #         self.i = 3
+        # if(self.i == 3):
+        #     actions[3] = Actions.GoToPointIgnore(field.ally_goal.frw_down, 1)
+        #     if((field.allies[3].get_pos() - field.ally_goal.frw_down).mag() < 250):
+        #         self.i = 0
+        
+        c = (aux.dist(field.enemies[0].get_pos(), field.allies[0].get_pos()))/3
+        d = aux.point_on_line(field.allies[0].get_pos(), field.enemies[0].get_pos(), c)
+        print(c)
+        u = aux.rotate(d, 3.14/2)
+        if(self.i == 0):
+            actions[3] = Actions.GoToPointIgnore(u + field.allies[3].get_pos(), 1)
+            if((field.allies[3].get_pos() - (u + field.allies[3].get_pos())).mag() < 20):
+                u = aux.rotate(d, 3.14/2 * 2)
+                self.i = 1
+        if(self.i == 1):
+            actions[3] = Actions.GoToPointIgnore(u, 1)
+            if((field.allies[3].get_pos() - (u + field.allies[3].get_pos())).mag() < 20):
+                u = aux.rotate(u, 3.14/2 * 3)
+                self.i = 2
+            actions[3] = Actions.GoToPointIgnore(u, 1)
+
+
+
+
+
+        
 
