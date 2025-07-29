6861af2eb0af21e0cc6f29effe42609320153a63
AL <alena7gur@gmail.com>
2025-07-29 15:26:47 +0300

    neymar2

--- bridge/strategy/AttakerNeymar.py
+++ bridge/strategy/AttakerNeymar.py
@@ -40,26 +40,26 @@ class Attaker_Neymar:
         r = (aux.point_on_line(field.allies[0].get_pos(), field.enemies[0].get_pos(), 300))
         u = (aux.point_on_line(field.allies[5].get_pos(), field.enemies[0].get_pos(), 300))
         if((y - l) > (l - p)):
-            actions[3] = Actions.GoToPointIgnore(r , (field.ball.get_pos() - field.allies[3].get_pos()).arg())
+            actions[self.idxN] = Actions.GoToPointIgnore(r , (field.ball.get_pos() - field.allies[self.idxN].get_pos()).arg())
         else:
-            actions[3] = Actions.GoToPointIgnore(u , (field.ball.get_pos() - field.allies[3].get_pos()).arg())
+            actions[self.idxN] = Actions.GoToPointIgnore(u , (field.ball.get_pos() - field.allies[self.idxN].get_pos()).arg())
 
     def block_pass(self, field: fld.Field, actions: list[Optional[Action]]) -> None:
-        actions[3] = Actions.GoToPointIgnore((aux.point_on_line(field.allies[0].get_pos(), field.enemies[0].get_pos(), 300)) , (field.ball.get_pos() - field.allies[3].get_pos()).arg())
+        actions[self.idxN] = Actions.GoToPointIgnore((aux.point_on_line(field.allies[0].get_pos(), field.enemies[0].get_pos(), 300)) , (field.ball.get_pos() - field.allies[3].get_pos()).arg())
 
     def opening_to_the_ball(self, field: fld.Field, actions: list[Optional[Action]]) -> None:
-        y = (field.ball.get_pos() - field.allies[2].get_pos()).unity()*50000
+        y = (field.ball.get_pos() - field.allies[self.idxN].get_pos()).unity()*50000
         b = -1
         for b in range(6):
             b+=1
-            if(aux.dist(aux.closest_point_on_line(field.ball.get_pos(), field.allies[2].get_pos(), field.enemies[b].get_pos(), "S"), field.enemies[b].get_pos()) < 100):
-                e = aux.get_angle_between_points(field.allies[2].get_pos(), field.ball.get_pos(), field.enemies[b].get_pos())
+            if(aux.dist(aux.closest_point_on_line(field.ball.get_pos(), field.allies[self.idxN].get_pos(), field.enemies[b].get_pos(), "S"), field.enemies[b].get_pos()) < 100):
+                e = aux.get_angle_between_points(field.allies[self.idxN].get_pos(), field.ball.get_pos(), field.enemies[b].get_pos())
                 if(e<0):
-                    while(aux.dist(aux.closest_point_on_line(field.ball.get_pos(), field.allies[2].get_pos(), field.enemies[b].get_pos(), "S"), field.enemies[b].get_pos()) < 100):
-                        actions[2] = Actions.GoToPointIgnore(aux.rotate(y, 3.14/2), (field.ball.get_pos() - field.allies[2].get_pos()).arg())
+                    while(aux.dist(aux.closest_point_on_line(field.ball.get_pos(), field.allies[self.idxN].get_pos(), field.enemies[b].get_pos(), "S"), field.enemies[b].get_pos()) < 100):
+                        actions[self.idxN] = Actions.GoToPointIgnore(aux.rotate(y, 3.14/2), (field.ball.get_pos() - field.allies[self.idxN].get_pos()).arg())
                 else:
-                    while(aux.dist(aux.closest_point_on_line(field.ball.get_pos(), field.allies[2].get_pos(), field.enemies[b].get_pos(), "S"), field.enemies[b].get_pos()) < 100):
-                        actions[2] = Actions.GoToPointIgnore(aux.rotate(y, -3.14/2), (field.ball.get_pos() - field.allies[2].get_pos()).arg())
+                    while(aux.dist(aux.closest_point_on_line(field.ball.get_pos(), field.allies[self.idxN].get_pos(), field.enemies[b].get_pos(), "S"), field.enemies[b].get_pos()) < 100):
+                        actions[self.idxN] = Actions.GoToPointIgnore(aux.rotate(y, -3.14/2), (field.ball.get_pos() - field.allies[self.idxN].get_pos()).arg())
     
     def run_Neymar(self, field: fld.Field, actions: list[Optional[Action]]) -> None:
         #self.pass_Ronaldo(field, actions)
